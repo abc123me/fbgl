6bee1473eadef8f0566d6cabf29434b9789c0b23
Jeremiah Lowe <jeremiah_abc123me@hotmail.com>
2018-06-24 01:00:41 -0400

    Rewrote to be more configurable

--- fbgl.cpp
+++ fbgl.cpp
@@ -3,34 +3,37 @@
 
 #include "inttypes.h"
 
-uint16_t SCREEN_WIDTH = 160;
-uint16_t SCREEN_HEIGHT = 128;
-uint8_t RED_BITS = 5;
-uint8_t GREEN_BITS = 6;
-uint8_t BLUE_BITS = 5;
-uint8_t BYTES_PER_PIXEL = 2;
+class FrameBuffer{
+	private:
+		uint16_t width, height;
+		uint8_t redBits, greenBits, blueBits;
+		uint8_t pixelSize;
+		int frameBuf;
 
-uint32_t makeColorRGB(float r, float g, float b);
-uint32_t makeColorRGB(float grey);
-uint32_t makeColorHSV(float h, float s, float v);
+		void corners(uint32_t color, uint16_t xa, uint16_t ya, uint16_t xs, uint16_t ys, bool fill);
+		void midpointEllipse(uint32_t color, uint16_t x, uint16_t y, uint16_t w, uint16_t h, bool fill);
+	public:
+		FrameBuffer(char* name, uint16_t width, uint16_t height);
+		FrameBuffer(char* name, uint16_t width, uint16_t height, uint8_t redBits, uint8_t greenBits, uint8_t blueBits, uint8_t pixelSize);
 
-void setPixel(int frameBuf, uint32_t color, uint16_t x, uint16_t y);
-void pushPixel(int frameBuf, uint32_t color);
+		uint32_t rgb(float r, float g, float b);
+		uint32_t rgb(float grey);
+		uint32_t hsv(float h, float s, float v);
 
-void fillRect(int frameBuf, uint32_t color, uint16_t x, uint16_t y, uint16_t w, uint16_t h);
-void drawRect(int frameBuf, uint32_t color, uint16_t x, uint16_t y, uint16_t w, uint16_t h);
-void line(int frameBuf, uint32_t color, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);
-void verticalLine(int frameBuf, uint32_t color, uint16_t y);
-void horizontalLine(int frameBuf, uint32_t color, uint16_t x);
-void drawEllipse(int frameBuf, uint32_t color, uint16_t xs1, uint16_t ys1, uint16_t rx, uint16_t ry);
-void fillEllipse(int frameBuf, uint32_t color, uint16_t xc, uint16_t yc, uint16_t rx, uint16_t ry);
-void background(int frameBuf, uint32_t color);
+		void setPixel(uint32_t color, uint16_t x, uint16_t y);
+		void pushPixel(uint32_t color);
+		void fillRect(uint32_t color, uint16_t x, uint16_t y, uint16_t w, uint16_t h);
+		void drawRect(uint32_t color, uint16_t x, uint16_t y, uint16_t w, uint16_t h);
+		void line(uint32_t color, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);
+		void verticalLine(uint32_t color, uint16_t y);
+		void horizontalLine(uint32_t color, uint16_t x);
+		void drawEllipse(uint32_t color, uint16_t xs1, uint16_t ys1, uint16_t rx, uint16_t ry);
+		void fillEllipse(uint32_t color, uint16_t xc, uint16_t yc, uint16_t rx, uint16_t ry);
+		void background(uint32_t color);
+		uint32_t getPosition(uint16_t x, uint16_t y);
+		void setPosition(uint16_t x, uint16_t y);
+		void toBeginning();
 
-uint32_t getPosition(uint16_t x, uint16_t y);
-void setPosition(uint16_t x, uint16_t y);
-void toBeginning(int frameBuf);
-
-int openFramebufferLinux(uint8_t id);
-void closeFramebuffer(int frameBuf);
-
+		void destroy();
+};
 #endif
--- fbgl.h
+++ fbgl.h
@@ -13,26 +13,44 @@
 #include "inttypes.h"
 #include "stdio.h"
 
-void corners(int frameBuf, uint32_t color, uint16_t xs, uint16_t ys, uint16_t x, uint16_t y, bool fill){
-	setPixel(frameBuf, color, xs + x, ys + y);
-	setPixel(frameBuf, color, xs - x, ys - y);
-	setPixel(frameBuf, color, xs - x, ys + y);
-	setPixel(frameBuf, color, xs + x, ys - y);
+FrameBuffer::FrameBuffer(char* name, uint16_t width, uint16_t height){
+	this->frameBuf = open(name, O_RDWR);
+	this->width = width;
+	this->height = height;
+	this->redBits = 8;
+	this->blueBits = 8;
+	this->greenBits = 8;
+	this->pixelSize = 4;
+}
+FrameBuffer::FrameBuffer(char* name, uint16_t width, uint16_t height, uint8_t redBits, uint8_t greenBits, uint8_t blueBits, uint8_t pixelSize){
+	this->frameBuf = open(name, O_RDWR);
+	this->width = width;
+	this->height = height;
+	this->redBits = redBits;
+	this->blueBits = blueBits;
+	this->greenBits = greenBits;
+	this->pixelSize = pixelSize;
+}
+void FrameBuffer::corners(uint32_t color, uint16_t xs, uint16_t ys, uint16_t x, uint16_t y, bool fill){
+	setPixel(color, xs + x, ys + y);
+	setPixel(color, xs - x, ys - y);
+	setPixel(color, xs - x, ys + y);
+	setPixel(color, xs + x, ys - y);
 	if(fill){
 		lseek(frameBuf, getPosition(xs - x, ys + y), SEEK_SET);
 		for(int i = xs - x; i < xs + x; i++)
-			write(frameBuf, &color, BYTES_PER_PIXEL);
+			write(frameBuf, &color, pixelSize);
 		lseek(frameBuf, getPosition(xs - x, ys - y), SEEK_SET);
 		for(int i = xs - x; i < xs + x; i++)
-			write(frameBuf, &color, BYTES_PER_PIXEL);
+			write(frameBuf, &color, pixelSize);
 	}
 }
-void midpointEllipse(int frameBuf, uint32_t color, uint16_t xc, uint16_t yc, uint16_t rx, uint16_t ry, bool fill){
+void FrameBuffer::midpointEllipse(uint32_t color, uint16_t xc, uint16_t yc, uint16_t rx, uint16_t ry, bool fill){
 	const uint32_t rx2 = rx * rx, ry2 = ry * ry;
 	int32_t x = 0, y = ry;
 	int32_t p = ry2 - (rx2 * ry) + (rx2 / 4);
 	while((2 * x * ry2) < (2 * y * rx2)){
-		corners(frameBuf, color, xc, yc, x, y, fill);
+		corners(color, xc, yc, x, y, fill);
 		if(p < 0){
 			x++;
 			p += (2 * ry2 * x) + ry2;
@@ -46,7 +64,7 @@ void midpointEllipse(int frameBuf, uint32_t color, uint16_t xc, uint16_t yc, uin
 	float py = y * y - 2 * y + 1.0f;
 	p = (px * ry2) + (py * rx2) - (rx2 * ry2);
 	while(y >= 0){
-		corners(frameBuf, color, xc, yc, x, y, fill);
+		corners(color, xc, yc, x, y, fill);
 		if(p > 0){
 			y--;
 			p -= (2 * rx2 * y) + rx2;
@@ -57,16 +75,16 @@ void midpointEllipse(int frameBuf, uint32_t color, uint16_t xc, uint16_t yc, uin
 		}
 	}
 }
-void fillEllipse(int frameBuf, uint32_t color, uint16_t xc, uint16_t yc, uint16_t rx, uint16_t ry){
-	midpointEllipse(frameBuf, color, xc, yc, rx, ry, true);
+void FrameBuffer::fillEllipse(uint32_t color, uint16_t xc, uint16_t yc, uint16_t rx, uint16_t ry){
+	midpointEllipse(color, xc, yc, rx, ry, true);
 }
-void drawEllipse(int frameBuf, uint32_t color, uint16_t xc, uint16_t yc, uint16_t rx, uint16_t ry){
-	midpointEllipse(frameBuf, color, xc, yc, rx, ry, false);
+void FrameBuffer::drawEllipse(uint32_t color, uint16_t xc, uint16_t yc, uint16_t rx, uint16_t ry){
+	midpointEllipse(color, xc, yc, rx, ry, false);
 }
-void line(int frameBuf, uint32_t color, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2){
+void FrameBuffer::line(uint32_t color, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2){
 	if(x1 == x2 && y1 == y2){
 		lseek(frameBuf, getPosition(x1, y1), SEEK_SET);
-		write(frameBuf, &color, BYTES_PER_PIXEL);
+		write(frameBuf, &color, pixelSize);
 		return;
 	}
 	if(x1 == x2){
@@ -74,7 +92,7 @@ void line(int frameBuf, uint32_t color, uint16_t x1, uint16_t y1, uint16_t x2, u
 		uint16_t ymin = y1 < y2 ? y1 : y2;
 		for (int y = ymin; y < ymax; y++){
 			lseek(frameBuf, getPosition(x1, y), SEEK_SET);
-			write(frameBuf, &color, BYTES_PER_PIXEL);
+			write(frameBuf, &color, pixelSize);
 		}
 		return;
 	}
@@ -83,91 +101,86 @@ void line(int frameBuf, uint32_t color, uint16_t x1, uint16_t y1, uint16_t x2, u
 		uint16_t xmin = x1 < x2 ? x1 : x2;
 		lseek(frameBuf, getPosition(xmin, y1), SEEK_SET);
 		for (int x = xmin; x < xmax; x++)
-			write(frameBuf, &color, BYTES_PER_PIXEL);
+			write(frameBuf, &color, pixelSize);
 		return;
 	}
 	int32_t vx = x2 - x1, vy = y2 - y1;
 	float inc = 1 / sqrt(vx * vx + vy * vy);
 	for(float f = 0; f < 1; f += inc){
 		lseek(frameBuf, getPosition(x1 + vx * f, y1 + vy * f), SEEK_SET);
-		write(frameBuf, &color, BYTES_PER_PIXEL);
+		write(frameBuf, &color, pixelSize);
 	}
 }
-void horizontalLine(int frameBuf, uint32_t color, uint16_t x){
+void FrameBuffer::horizontalLine(uint32_t color, uint16_t x){
 	lseek(frameBuf, getPosition(x, 0), SEEK_SET);
-	for(int i = 0; i < SCREEN_HEIGHT; i++){
-		write(frameBuf, &color, BYTES_PER_PIXEL);
-		lseek(frameBuf, (SCREEN_WIDTH - 1) * BYTES_PER_PIXEL, SEEK_CUR);
+	for(int i = 0; i < height; i++){
+		write(frameBuf, &color, pixelSize);
+		lseek(frameBuf, (width - 1) * pixelSize, SEEK_CUR);
 	}
 }
-void verticalLine(int frameBuf, uint32_t color, uint16_t y){
+void FrameBuffer::verticalLine(uint32_t color, uint16_t y){
 	lseek(frameBuf, getPosition(0, y), SEEK_SET);
-	for(int i = 0; i < SCREEN_WIDTH; i++)
-		write(frameBuf, &color, BYTES_PER_PIXEL);
+	for(int i = 0; i < width; i++)
+		write(frameBuf, &color, pixelSize);
 }
-void background(int frameBuf, uint32_t color){
+void FrameBuffer::background(uint32_t color){
 	lseek(frameBuf, 0, SEEK_SET);
-	for(int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++)
-		write(frameBuf, &color, BYTES_PER_PIXEL);
+	for(int i = 0; i < width * height; i++)
+		write(frameBuf, &color, pixelSize);
 }
-void fillRect(int frameBuf, uint32_t color, uint16_t x, uint16_t y, uint16_t w, uint16_t h){
-	uint16_t y2 = h + y, y2d = y2 - 1;
-	uint16_t x2 = w + x, x2d = x2 - 1;
+void FrameBuffer::fillRect(uint32_t color, uint16_t x, uint16_t y, uint16_t w, uint16_t h){
+	uint16_t y2 = h + y;
+	uint16_t x2 = w + x;
 	lseek(frameBuf, getPosition(x, y), SEEK_SET);
 	for(int py = y; py < y2; py++){
 		lseek(frameBuf, getPosition(x, py), SEEK_SET);
 		for(int px = x; px < x2; px++)
-			write(frameBuf, &color, BYTES_PER_PIXEL);
+			write(frameBuf, &color, pixelSize);
 	}
 }
-void drawRect(int frameBuf, uint32_t color, uint16_t x, uint16_t y, uint16_t w, uint16_t h){
+void FrameBuffer::drawRect(uint32_t color, uint16_t x, uint16_t y, uint16_t w, uint16_t h){
 	uint16_t y2 = h + y, y2d = y2 - 1;
-	uint16_t x2 = w + x, x2d = x2 - 1;
+	uint16_t x2 = w + x;
 	for(int py = y; py < y2; py++){
 		lseek(frameBuf, getPosition(x, py), SEEK_SET);
 		if(py == y || py == y2d)
 			for(int px = x; px < x2; px++)
-				write(frameBuf, &color, BYTES_PER_PIXEL);
+				write(frameBuf, &color, pixelSize);
 		else{
-			write(frameBuf, &color, BYTES_PER_PIXEL);
-			lseek(frameBuf, BYTES_PER_PIXEL * (w - 2), SEEK_CUR);
-			write(frameBuf, &color, BYTES_PER_PIXEL);
+			write(frameBuf, &color, pixelSize);
+			lseek(frameBuf, pixelSize * (w - 2), SEEK_CUR);
+			write(frameBuf, &color, pixelSize);
 		}
 	}
 }
-void setPixel(int frameBuf, uint32_t color, uint16_t x, uint16_t y){
+void FrameBuffer::setPixel(uint32_t color, uint16_t x, uint16_t y){
 	lseek(frameBuf, getPosition(x, y), SEEK_SET);
-	write(frameBuf, &color, BYTES_PER_PIXEL);
+	write(frameBuf, &color, pixelSize);
 }
-uint32_t getPosition(uint16_t x, uint16_t y){
-	return BYTES_PER_PIXEL * (x + y * SCREEN_WIDTH);
+uint32_t FrameBuffer::getPosition(uint16_t x, uint16_t y){
+	return pixelSize * (x + y * width);
 }
-uint32_t makeColorRGB(float rp, float gp, float bp){
-	#ifdef _DEBUG_MODE
-		if(rp < 0 || rp > 1){ fprintf(stderr, "Red pixel value is invalid: %f", rp); exit(-1); }
-		if(gp < 0 || gp > 1){ fprintf(stderr, "Green pixel value is invalid: %f", rp); exit(-1); }
-		if(bp < 0 || bp > 1){ fprintf(stderr, "Blue pixel value is invalid: %f", rp); exit(-1); }
-	#endif
+uint32_t FrameBuffer::rgb(float rp, float gp, float bp){
 	static int64_t RMAX = -1, BMAX = -1, GMAX = -1;
-	if(RMAX < 0) RMAX = pow(2, RED_BITS) - 1;
-	if(GMAX < 0) GMAX = pow(2, GREEN_BITS) - 1;
-	if(BMAX < 0) BMAX = pow(2, BLUE_BITS) - 1;
+	if(RMAX < 0) RMAX = pow(2, redBits) - 1;
+	if(GMAX < 0) GMAX = pow(2, greenBits) - 1;
+	if(BMAX < 0) BMAX = pow(2, blueBits) - 1;
 	uint32_t r = rp * RMAX;
-	r <<= BLUE_BITS + GREEN_BITS;
+	r <<= blueBits + greenBits;
 	uint16_t g = gp * GMAX;
-	g <<= BLUE_BITS;
+	g <<= blueBits;
 	uint8_t b = bp * BMAX;
 	return r + g + b;
 }
-uint32_t makeColorRGB(float grey){
-	return makeColorRGB(grey, grey, grey);
+uint32_t FrameBuffer::rgb(float grey){
+	return rgb(grey, grey, grey);
 }
-uint32_t makeColorHSV(float h, float s, float v){
+uint32_t FrameBuffer::hsv(float h, float s, float v){
 	float hh, p, q, t, ff;
 	int32_t i;
 
 	if(s <= 0.0)
-		return makeColorRGB(v, v, v);
+		return rgb(v, v, v);
 	hh = h;
 	while(hh >= 360.0)
 		hh -= 360.0;
@@ -181,32 +194,25 @@ uint32_t makeColorHSV(float h, float s, float v){
 	t = v * (1.0 - (s * (1.0 - ff)));
 
 	switch(i) {
-		case 0: return makeColorRGB(v, t, p);
-		case 1: return makeColorRGB(q, v, p);
-		case 2: return makeColorRGB(p, v, t);
-		case 3: return makeColorRGB(p, q, v);
-		case 4: return makeColorRGB(t, p, v);
+		case 0: return rgb(v, t, p);
+		case 1: return rgb(q, v, p);
+		case 2: return rgb(p, v, t);
+		case 3: return rgb(p, q, v);
+		case 4: return rgb(t, p, v);
 	}
-	return makeColorRGB(v, p, q);
+	return rgb(v, p, q);
 }
 
-int openFramebufferLinux(uint8_t id){
-	int fd;
-	char data[16];
-	sprintf(data, "/dev/fb%i", id);
-	fd = open(data, O_RDWR);
-	return fd;
-}
-void closeFramebuffer(int frameBuf){
+void FrameBuffer::destroy(){
 	close(frameBuf);
 }
-void toBeginning(int frameBuf){
+void FrameBuffer::toBeginning(){
 	lseek(frameBuf, 0, SEEK_SET);
 }
-void pushPixel(int frameBuf, uint32_t color){
-	write(frameBuf, &color, BYTES_PER_PIXEL);
+void FrameBuffer::pushPixel(uint32_t color){
+	write(frameBuf, &color, pixelSize);
 }
-void setPosition(int frameBuf, uint16_t x, uint16_t y){
+void FrameBuffer::setPosition(uint16_t x, uint16_t y){
 	lseek(frameBuf, getPosition(x, y), SEEK_SET);
 }
 
